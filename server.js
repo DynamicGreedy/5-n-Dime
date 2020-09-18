94596c0261745b62b58c083ef9177f4404f9aabc
Roopam-mishra <roopammishra105@gmail.com>
2020-09-19 02:10:34 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path=require('path');
+const port = process.env.PORT || 3000;
+app.use('/', require('./routes/users.js'));
+app.listen(port,()=>{
+    console.log(`Server is running at http://localhost:${port}`);
+});
