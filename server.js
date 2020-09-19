6894027746924338be9e1a0d0f866bb5dcaca0ba
Pranjul Pal <pranjulpal22@gmail.com>
2020-09-19 14:16:33 +0530

    Updated register and login backend

--- server.js
+++ server.js
@@ -3,12 +3,47 @@ const bodyParser = require('body-parser');
 const path=require('path');
 const app = express();
 const port = process.env.PORT || 7000;
+const flash = require('connect-flash');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const passport = require('passport');
+require('dotenv').config();
+
+// Passport Config
+require('./config/passport')(passport);
+mongoose.set('useFindAndModify', false);
+const uri = process.env.mongodburl;
+mongoose.connect(uri,{ useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify: false});
 
 app.set('view engine' , 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname + '/public')));
+
+// Express session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 app.use('/', require('./routes/users.js'));
 app.listen(port,()=>{
