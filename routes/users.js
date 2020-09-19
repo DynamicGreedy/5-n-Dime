b84cbf280290d82961e31e411bbe7db3329b2e09
Pranjul Pal <pranjulpal22@gmail.com>
2020-09-19 16:29:46 +0530

    Updated addshop route

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const { forwardAuthenticated } = require('../config/auth');
 const { ensureAuthenticated } = require('../config/auth');
 const User = require('../models/User');
+const Shopowner = require('../models/Shopowner');
 
 router.get('/' , (req , res) => res.render('index' , {user: req.user}));
 
@@ -110,6 +111,83 @@ router.post('/formlogin', (req, res, next) => {
 router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
+});
+
+//Add Shop
+router.get('/formaddshop',ensureAuthenticated, (req, res) => res.render('formaddshop'));
+router.post('/formaddshop',ensureAuthenticated, (req, res) =>{
+    // console.log(req.user);
+    const { shopname, ownername,  shoptype, donationwant , aboutshop , pincode , area } = req.body;
+    // console.log(req.body.ownername);
+    // console.log(req.body.shopname);
+    // console.log(req.body.shoptype);
+    // console.log(req.body.donationwant);
+    // console.log(req.body.aboutshop);
+    let errors = [];
+
+    if (!shopname || !ownername || !shoptype || !donationwant || !aboutshop || !pincode || !area) 
+    {
+        errors.push({ msg: 'Please enter all fields' });
+    }
+    if(pincode.length != 6) errors.push({msg: "Pincode is not valid"});
+    if (errors.length > 0) {
+        res.render('formaddshop', {
+            errors,
+            shopname,
+            ownername,  
+            shoptype,
+            donationwant,
+            aboutshop,
+            pincode,
+            area
+        });
+    }
+    else
+    {
+        Shopowner.findOne({ shopname:shopname }).then(shopowner => 
+        {
+            if (shopowner) 
+            {
+                errors.push({ msg: 'Shop name already exists' });
+                res.render('formaddshop', {
+                    errors,
+                    shopname,
+                    ownername,
+                shoptype,
+                donationwant,
+                aboutshop,
+                pincode,
+                area
+                });
+            } 
+            else
+            {
+                //console.log(req.user);
+                req.user.addShop = true;
+                req.user.save();
+                //console.log(req.user);
+                const newShopowner = new Shopowner({
+                    email: req.user.email,
+                    ownername,
+                    shopname,
+                    ownername,
+                    shoptype,
+                    donationwant,
+                    aboutshop,
+                    pincode,
+                    area,
+                });
+                newShopowner.save().then(shopowner=>{
+                    req.flash(
+                        'success_msg',
+                        'Your shop has been added successfully'
+                      );
+                    res.redirect('/');
+                })
+                .catch(err => console.log(err));
+            }
+        });
+    }
 });
 
 module.exports = router;
