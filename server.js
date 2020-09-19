7608ddc89723ae1e8043e3bfc020f075df03fe61
Pranjul Pal <pranjulpal22@gmail.com>
2020-09-19 11:49:19 +0530

    Updated users routes and server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path=require('path');
+const app = express();
 const port = process.env.PORT || 3000;
+
+app.set('view engine' , 'ejs');
+
 app.use('/', require('./routes/users.js'));
 app.listen(port,()=>{
     console.log(`Server is running at http://localhost:${port}`);
