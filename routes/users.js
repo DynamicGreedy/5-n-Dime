12678f76b63bb691a113340a8b945521fb70d679
Roopam-mishra <roopammishra105@gmail.com>
2020-09-19 12:20:20 +0530

    added login register form

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
 router.get('/' , (req , res) => res.render('index'));
-
+// Register Page
+router.get('/formregister',(req, res) => res.render('formregister'));
+// Login Page
+router.get('/formlogin', (req, res) => res.render('formlogin'));
 module.exports = router;
